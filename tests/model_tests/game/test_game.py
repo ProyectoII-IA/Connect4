62d04be4c914f4f88100b7ada3a8c65b93278c19
Mariana <mari.semeraro27@gmail.com>
2018-11-22 15:06:48 -0600

    Test game issue

--- tests/model_tests/game/test_game.py
+++ tests/model_tests/game/test_game.py
@@ -2,6 +2,9 @@ from ....model.game.game import Game
 from ....model.game.human import Human
 from ....model.game.agent import Agent
 
+player_1 = Human()
+player_2 = Agent(2, 1) # 2 symbol of the agent, 1 symbol of the opponent
+game = Game(player_1, player_2)
 
 def test_play_game():
     """ Check that game is working correctly
@@ -16,10 +19,6 @@ def test_turn():
 def test_who_is_winner_player_1():
     """ Check who is the winner of the game
     """
-
-    player_1 = Human()
-    player_2 = Agent(2, 1) # 2 es el symb del agente, 1 el del oponente
-    game = Game(player_1, player_2)
     game.turn_player_1 = True
     game.board.created_board()
     winner = "** Ganador Jugador 1 **"
@@ -29,10 +28,6 @@ def test_who_is_winner_player_1():
 def test_who_is_winner_player_2():
     """ Check who is the winner of the game
     """
-
-    player_1 = Human()
-    player_2 = Agent(2, 1) # 2 es el symb del agente, 1 el del oponente
-    game = Game(player_1, player_2)
     game.turn_player_1 = False
     game.board.created_board()
     winner = "** Ganador Jugador 2 **"
@@ -42,16 +37,12 @@ def test_who_is_winner_player_2():
 def test_is_winner_True():
     """ Check if exist a winner
     """
-    player_1 = Human()
-    player_2 = Agent(2, 1)
-    game = Game(player_1, player_2)
     array = [[1,1,1,2,1,0,0],
              [2,1,1,1,2,0,0],
              [0,0,1,2,2,0,0],
              [0,0,1,2,1,0,0],
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0]]
-
     game.board.last_mov = (3,2)
     game.turn_player_1 = True
     game.board.board = array
@@ -61,16 +52,12 @@ def test_is_winner_True():
 def test_is_winner_False():
     """ Check if exist a winner
     """
-    player_1 = Human()
-    player_2 = Agent(2, 1)
-    game = Game(player_1, player_2)
     array = [[1,1,1,2,1,0,0],
              [2,1,1,1,2,0,0],
              [0,0,2,2,2,0,0],
              [0,0,1,2,1,0,0],
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0]]
-
     game.board.last_mov = (3,2)
     game.turn_player_1 = True
     game.board.board = array
