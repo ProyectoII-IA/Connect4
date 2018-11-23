7be8f2cedbe22d0eda6046d1f9fec99c87dd6781
kanatalia1995 <kanatalia95@gmail.com>
2018-11-22 21:16:34 -0600

    MAIN MODIFIED TO TEST SEQUENTIAL STRATEGY Note: set the probability to check both strategies, use 0.01 to test Space and 0.99 to test sequential

--- main_game.py
+++ main_game.py
@@ -1,23 +1,35 @@
 from model.game.board import Board
-
+from model.strategies.position  import Number
+from model.strategies.sequential_vs_space import SequentialvsSpace
 def main():
-    b = [[1,0,1,0,0,0,1],
-         [1,0,0,0,0,0,1],
-         [1,0,0,0,0,1,1],
-         [1,0,0,1,0,0,1],
-         [1,1,0,0,0,0,1],
-         [1,0,0,0,1,0,1]]
+    b = [[1,0,1,1,0,0,0],
+         [0,0,0,1,0,0,0],
+         [0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0]]
 
 
     board = Board()
     board.board = b
 
+    array_number = []
+    for i in range(0,7):
+        var_number = Number(i)
+        array_number.append(var_number)
+    
+    strategy = SequentialvsSpace(0.80,1)
+    var_action = strategy.get_action(board,array_number)
+    print(var_action)
+    for x in var_action: 
+        print(x.position,x.amount)
+        
  
 
     print(board.get_empty_element(0))
-    print(board.get_column(0))
-    print(board.get_cells_symbol(1))
-    print(board.is_fill_column(6))
+    print(board.get_column(1))
+    print(board.get_cells_symbol(2))
+    print(board.is_fill_column(5))
 
 
 if __name__ == "__main__":
