78e2469a86121fd980128ce8457874a33acb0255
Mariana <mari.semeraro27@gmail.com>
2018-11-24 19:33:19 -0600

    Test game and player issues

--- tests/model_tests/game/test_player.py
+++ tests/model_tests/game/test_player.py
@@ -11,24 +11,35 @@
 from model.game.player import Player
 from model.game.board import Board
 
-array = [[1, 1, 1, 2, 1, 0, 0],
-         [2, 1, 1, 1, 2, 0, 0],
-         [0, 0, 1, 2, 2, 0, 0],
-         [0, 0, 0, 2, 1, 0, 0],
-         [0, 0, 0, 1, 0, 0, 0],
-         [0, 0, 0, 2, 0, 0, 0]]
-board = Board()
-board.set_board(array)
-player_1 = Player()
 
 def test_validate_action_True():
     """ Check that validate_action is working correctly
     """
+    array = [[1, 1, 1, 2, 1, 0, 0],
+             [2, 1, 1, 1, 2, 0, 0],
+             [0, 0, 1, 2, 2, 0, 0],
+             [0, 0, 0, 2, 1, 0, 0],
+             [0, 0, 0, 1, 0, 0, 0],
+             [0, 0, 0, 2, 0, 0, 0]]
+    board = Board()
+    board.set_board(array)
+    player_1 = Player()
     result = player_1.validate_action(board, 0)
+    result_2 = player_1.validate_action(board, 3)
     assert(result)
+    assert(not result_2)
 
 def test_validate_action_False():
     """ Check that validate_action is working correctly
     """
-    result = player_1.validate_action(board, 3)
+    array = [[1, 1, 1, 2, 1, 0, 0],
+             [2, 1, 1, 1, 2, 0, 0],
+             [0, 0, 1, 2, 2, 0, 0],
+             [0, 0, 0, 2, 1, 0, 0],
+             [0, 0, 0, 1, 0, 0, 0],
+             [0, 0, 0, 2, 0, 0, 0]]
+    board = Board()
+    board.set_board(array)
+    player_1 = Player()
+    result = player_1.validate_action(board, 9)
     assert(not result)
